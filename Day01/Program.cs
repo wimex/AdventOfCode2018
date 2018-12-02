682fc740962f29a18933dc84c74d463a8c27c1cf
wimex <marton2@gmail.com>
2018-12-02 23:56:39 +0100

    Fix some issues

---
+++ Day01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input1.txt");
+            List<int> history = new List<int>();
+            int current = 0;
+
+            while (true) {
+                foreach (string line in lines)
+                {
+                    int number = int.Parse(line.Substring(1));
+                    if (line[0] == '+')
+                        current += number;
+                    else
+                        current -= number;
+
+                    if (history.Contains(current))
+                        goto found;
+                    else
+                        history.Add(current);
+                }
+            }
+
+        found:
+            Console.WriteLine(string.Join(',', history) + "," + current);
+            Console.ReadKey();
+        }
+    }
+}
