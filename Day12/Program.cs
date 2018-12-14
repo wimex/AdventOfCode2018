c4b63087d16f55d35448535d3e966c614e148441
wimex <marton2@gmail.com>
2018-12-14 22:00:46 +0100

    Day 12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day12
@@ -12,7 +13,25 @@ namespace Day12
         static void Main(string[] args)
         {
             long generation = 0;
-            List<string> padding = new List<string> { ".", ".", ".", ".", "." };
+            List<string> padding = new List<string> {".", ".", ".", ".", "."};
+            /*List<string> plants = "#..#.#..##......###...###".Select(s => s.ToString()).ToList();
+            Dictionary<string, string> statechanges = new Dictionary<string, string>
+            {
+                {"...##", "#"},
+                {"..#..", "#"},
+                {".#...", "#"},
+                {".#.#.", "#"},
+                {".#.##", "#"},
+                {".##..", "#"},
+                {".####", "#"},
+                {"#.#.#", "#"},
+                {"#.###", "#"},
+                {"##.#.", "#"},
+                {"##.##", "#"},
+                {"###..", "#"},
+                {"###.#", "#"},
+                {"####.", "#"}
+            };*/
             List<string> plants = "#......##...#.#.###.#.##..##.#.....##....#.#.##.##.#..#.##........####.###.###.##..#....#...###.##".Select(s => s.ToString()).ToList();
             Dictionary<string, string> statechanges = new Dictionary<string, string>
             {
@@ -49,13 +68,19 @@ namespace Day12
                 {"####.", "#"},
                 {"###..", "#"},
             };
-            
-            while (generation < 20)
+
+            long iterations = 50000000000;
+            long index = 0;
+            long pindex = 0;
+            long summary = 0;
+            string history = null;
+            while (generation < iterations)
             {
-                List<string> next = new List<string>(plants);
-                Parallel.For(2, plants.Count - 2, (i) =>
+                List<string> padded = padding.Concat(plants).Concat(padding).ToList();
+                List<string> next = new List<string>(padded);
+                Parallel.For(2, padded.Count - 2, (i) =>
                 {
-                    string pattern = plants[i - 2] + plants[i - 1] + plants[i] + plants[i + 1] + plants[i + 2];
+                    string pattern = padded[i - 2] + padded[i - 1] + padded[i] + padded[i + 1] + padded[i + 2];
                     string statechange = statechanges.TryGetValue(pattern, out string nx) ? nx : ".";
 
                     next[i] = statechange;
@@ -64,21 +89,38 @@ namespace Day12
                 int padLeft = next.IndexOf("#");
                 int padRight = next.LastIndexOf("#");
 
-                plants = padding.Concat(next.Skip(padLeft - 1).Take(padRight - padLeft + 1).ToList()).Concat(padding).ToList();
+                plants = next.Skip(padLeft).Take(padRight - padLeft + 1).ToList();
+                pindex = index;
+                index += padding.Count - padLeft;
+                summary = GetPlantSummary(index, plants);
                 generation++;
-                
-                Console.WriteLine($"{string.Join("", plants)}");
-                Console.WriteLine($"@{generation}, size: {plants.Count}, plants: {plants.Count(p => p == "#")}");
-            }
 
+                bool stabilized = history == string.Join("", plants);
+                if (stabilized)
+                {
+                    index = index + (iterations - generation) * (index - pindex);
+                    summary = GetPlantSummary(index, plants);
+                    break;
+                }
+
+                Console.WriteLine($"{string.Join("", plants)}");
+                Console.WriteLine($"@{generation}, size: {plants.Count}, plants: {plants.Count(p => p == "#")}, index: {index}, summary: {summary}");
+                history = string.Join("", plants);
+            }
+            
+            Console.WriteLine($"Result: {summary}");
+            Console.ReadKey();
+        }
+
+        static long GetPlantSummary(long index, List<string> plants)
+        {
             long result = 0;
             for (int i = 0; i < plants.Count; i++)
             {
-              result += plants[i] == "#" ? (i - padding.Count ) : 0;
+                result += plants[i] == "#" ? (i - index) : 0;
             }
 
-            Console.WriteLine($"Result: {result}");
-            Console.ReadKey();
+            return result;
         }
     }
 }
