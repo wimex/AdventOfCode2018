c93efad8290d29c67a434fe686427939591f2dbc
Marton Balogh Bodor <marton.baloghbodor@logmein.com>
2018-12-14 18:06:45 +0100

    Update calculations

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,9 @@ namespace Day12
     {
         static void Main(string[] args)
         {
-            int padLeft = 3;
-            int padRight = 3;
-
             long generation = 0;
-            List<string> plants = (new string('.', padLeft) + "#......##...#.#.###.#.##..##.#.....##....#.#.##.##.#..#.##........####.###.###.##..#....#...###.##" + new string('.', padRight)).Select(s => s.ToString()).ToList();
+            List<string> padding = new List<string> { ".", ".", ".", ".", "." };
+            List<string> plants = "#......##...#.#.###.#.##..##.#.....##....#.#.##.##.#..#.##........####.###.###.##..#....#...###.##".Select(s => s.ToString()).ToList();
             Dictionary<string, string> statechanges = new Dictionary<string, string>
             {
                 {".#.##", "."},
@@ -50,43 +49,32 @@ namespace Day12
                 {"####.", "#"},
                 {"###..", "#"},
             };
-            Console.ReadKey();
-
-            while (generation < 50000000000)
+            
+            while (generation < 20)
             {
                 List<string> next = new List<string>(plants);
                 Parallel.For(2, plants.Count - 2, (i) =>
                 {
                     string pattern = plants[i - 2] + plants[i - 1] + plants[i] + plants[i + 1] + plants[i + 2];
-                    string statechange = statechanges.ContainsKey(pattern) ? statechanges[pattern] : plants[i];
+                    string statechange = statechanges.TryGetValue(pattern, out string nx) ? nx : ".";
 
                     next[i] = statechange;
                 });
 
-                if (next[0] == "#" || next[1] == "#" || next[2] == "#")// || next[3] == "#" || next[4] == "#")
-                {
-                    next.Insert(0, ".");
-                    padLeft++;
-                }
+                int padLeft = next.IndexOf("#");
+                int padRight = next.LastIndexOf("#");
 
-                if (next[next.Count - 1] == "#" || next[next.Count - 2] == "#" || next[next.Count - 3] == "#")// || next[next.Count - 4] == "#" || next[next.Count - 5] == "#")
-                {
-                    next.Add(".");
-                    padRight++;
-                }
-
-                plants = next;
+                plants = padding.Concat(next.Skip(padLeft - 1).Take(padRight - padLeft + 1).ToList()).Concat(padding).ToList();
                 generation++;
-
-                if (generation % 10000 == 0)
-                //Console.WriteLine($"{string.Join("", plants)}");
+                
+                Console.WriteLine($"{string.Join("", plants)}");
                 Console.WriteLine($"@{generation}, size: {plants.Count}, plants: {plants.Count(p => p == "#")}");
             }
 
-            int result = 0;
+            long result = 0;
             for (int i = 0; i < plants.Count; i++)
             {
-                result += plants[i] == "#" ? (i - padLeft) : 0;
+              result += plants[i] == "#" ? (i - padding.Count ) : 0;
             }
 
             Console.WriteLine($"Result: {result}");
